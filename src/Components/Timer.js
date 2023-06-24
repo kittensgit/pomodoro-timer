386c8c29bd280d639114828b282f925326f7942a
Nika <nikaonime@gmail.com>
2023-06-24 17:54:26 +0300

    useEffect, setInterval

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import 'react-circular-progressbar/dist/styles.css';
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import PlayButton from './Buttons/PlayButton';
@@ -8,7 +8,48 @@ import SettingsContext from './SettingsContext';
 
 const Timer = () => {
 
-    const { showSettings, setShowSettings } = useContext(SettingsContext)
+    const [isPaused, setIsPaused] = useState(false)
+    const [secondsLeft, setSecondsLeft] = useState(0)
+    const [mode, setMode] = useState('work') // work/break/null
+    const { workMinutes, breakMinutes, showSettings, setShowSettings } = useContext(SettingsContext)
+
+    const secondsLeftRef = useRef(secondsLeft)
+    const isPausedRef = useRef(isPaused)
+    const modeRef = useRef(mode)
+
+    const initTimer = () => {
+        setSecondsLeft(workMinutes * 60)
+    }
+
+    const switchMode = () => {
+        const nextMode = modeRef.current === 'work' ? 'break' : 'work';
+        const nextSeconds = (nextMode === 'work' ? workMinutes : breakMinutes) * 60
+        setMode(nextMode)
+        modeRef.current = nextMode
+        setSecondsLeft(nextSeconds)
+        secondsLeftRef.current = nextSeconds
+    }
+
+    const tick = () => {
+        secondsLeftRef.current--
+        setSecondsLeft(secondsLeftRef.current)
+    }
+
+    useEffect(() => {
+        initTimer()
+
+        const interval = setInterval(() => {
+            if (isPausedRef.current) {
+                return;
+            }
+            if (secondsLeftRef.current === 0) {
+                return switchMode()
+            }
+            tick()
+        }, 1000)
+
+        return clearInterval(interval);
+    }, [showSettings, setShowSettings])
 
     const red = '#f54e4e';
     const green = '#4aec8c'
@@ -25,8 +66,7 @@ const Timer = () => {
                 })}
             />
             <div style={{ marginTop: '20px' }}>
-                <PlayButton />
-                <PauseButton />
+                {isPaused ? <PlayButton /> : <PauseButton />}
             </div>
             <div style={{ marginTop: '20px' }}>
                 <SettingsButton onClick={() => setShowSettings(true)} />
