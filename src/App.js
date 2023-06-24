bdda20a9b310d29ded00c911577be1a30b43e4a0
Nika <nikaonime@gmail.com>
2023-06-24 16:47:55 +0300

    useState, breakMinutes, workMinutes, onChange

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ import SettingsContext from './Components/SettingsContext';
 function App() {
 
   const [showSettings, setShowSettings] = useState(true)
+  const [workMinutes, setWorkMinutes] = useState(45)
+  const [breakMinutes, setBreakMinutes] = useState(15)
 
   return (
     <main>
       <SettingsContext.Provider value={{
-        workMinutes: 45,
-        breakMinutes: 15
+        workMinutes,
+        breakMinutes,
+        setWorkMinutes,
+        setBreakMinutes
       }}>
         {showSettings ? <Settings /> : <Timer />}
       </SettingsContext.Provider>
--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -5,7 +5,7 @@ import SettingsContext from './SettingsContext'
 
 const Settings = () => {
 
-const {workMinutes, breakMinutes} = useContext(SettingsContext)
+    const { workMinutes, breakMinutes, setWorkMinutes, setBreakMinutes } = useContext(SettingsContext)
 
     return (
         <div style={{ textAlign: "left" }}>
@@ -15,15 +15,17 @@ const {workMinutes, breakMinutes} = useContext(SettingsContext)
                 thumbClassName='thumb' // это один из элементов ползунка, который можно использовать для перетаскивания и изменения выбранного значения ползунка.
                 trackClassName='track' //сам ползунок по которому перетпскиваем thumb
                 value={workMinutes}
+                onChange={newValue => setWorkMinutes(newValue)}
                 min={1}
                 max={120}
             />
             <label>Break: {breakMinutes}:00</label>
             <ReactSlider
                 className='slider green'
-                thumbClassName='thumb' 
+                thumbClassName='thumb'
                 trackClassName='track'
                 value={breakMinutes}
+                onChange={newValue => setBreakMinutes(newValue)}
                 min={1}
                 max={120}
             />
